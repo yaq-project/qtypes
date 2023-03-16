cf03637b7b109ead44a5fd83adec9abb9ea000b8
Blaise Thompson <blaise@untzag.com>
2023-03-16 18:40:56 -0500

    force widgets to ignore mouse wheel

--- qtypes/_widgets/_enum.py
+++ qtypes/_widgets/_enum.py
@@ -4,11 +4,19 @@ from qtpy import QtWidgets, QtGui, QtCore
 class Widget(QtWidgets.QComboBox):
     def __init__(self, model, parent):
         super().__init__(parent=parent)
+        self.setFocusPolicy(QtCore.Qt.StrongFocus)
+        self.installEventFilter(self)
         self.currentTextChanged.connect(self.on_current_text_changed)
         # wire into model
         self.model = model
         self.model.updated_connect(self.on_updated)
         self.on_updated(model.get())
+
+    def eventFilter(self, obj, event):
+        if isinstance(event, QtGui.QWheelEvent):
+            return True
+        else:
+            return super().eventFilter(obj, event)
 
     def updated_disconnect(self):
         self.model.updated_disconnect(self.on_updated)
